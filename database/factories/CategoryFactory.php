28689e1b003d56c93b8890330e66cb53e777f243
thenord <dmr11@bk.ru>
2019-01-06 12:43:25 +0300

    Создание фабрик

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $category = $faker->word,
+        'slug' => str_slug($category),
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\Category;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Question::class, function (Faker $faker) {
     return [
-        //
+        'title' => $title = $faker->sentence(),
+        'slug' => str_slug($title),
+        'body' => $faker->text,
+        'category_id' => function() {
+            return Category::all()->random();
+        },
+        'user_id' => function() {
+            return User::all()->random();
+        },
     ];
 });
